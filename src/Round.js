77cd3a0d15e833a3b1c6583b9ec8b3a553aa23c1
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-02-03 15:50:16 -0700

    Add final tests to round

--- src/Round.js
+++ src/Round.js
@@ -5,7 +5,9 @@ class Round {
     this.deck = deck;
     this.turn = 0;
     this.turnCount = 0
+    this.correct = 0
     this.wrongGuess = [];
+    this.isCorrect = true;
   }
   returnCurrentCard() {
     return this.deck.cards[this.turnCount]
@@ -13,12 +15,27 @@ class Round {
 
   takeTurn(playerGuess) {
     const turn = new Turn(playerGuess,     this.returnCurrentCard())
-    let result = turn.evaluateGuess()
     this.turnCount++
     if(turn.evaluateGuess() === false) {
       this.wrongGuess.push(this.turnCount)
+      this.isCorrect = false;
+      return 'incorrect!'
     }
-    return result
+    this.correct++
+    this.isCorrect = true;
+    return 'correct!'
+  }
+  calculatePercentCorrect() {
+    const amnt = (this.turnCount - this.wrongGuess.length) / this.turnCount
+
+    const sum = Math.floor(amnt * 100)
+    return sum
+  }
+  endRound() {
+    const resultMsg = `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+    console.log(resultMsg)
+    return resultMsg
+
   }
 }
 
